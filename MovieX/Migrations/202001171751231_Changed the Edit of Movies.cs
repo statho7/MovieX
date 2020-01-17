88b0462afc219454ca01f7b32dc589c6621fbb5b
andre <andreas.stathop@gmail.com>
2020-01-17 19:51:36 +0200

    Created folder Image and changed the Edit view of movies and did 3 Migrations

---
+++ MovieX/Migrations/202001171751231_Changed the Edit of Movies.cs
@@ -0,0 +1,16 @@
+namespace MovieX.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangedtheEditofMovies : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
