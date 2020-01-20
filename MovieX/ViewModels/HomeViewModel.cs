3159140eaf6d0bf6d8852859fcd579b3f9df3383
andre <andreas.stathop@gmail.com>
2020-01-20 23:38:17 +0200

    Edited the NavBar of Admin , changed the HomeViewModel & changed the _Layout and _LoginPartial of Views/Shared

--- MovieX/ViewModels/HomeViewModel.cs
+++ MovieX/ViewModels/HomeViewModel.cs
@@ -9,7 +9,6 @@ namespace MovieX.ViewModels
     public class HomeViewModel
     {
         public IEnumerable<Movie> Movies { get; set; }
-        //public IEnumerable<ApplicationUser> UsersList { get; set; }
         public bool UserPaid { get; set; }
     }
 }
